6d7d7166bca512028885c375ac52d7ab12e150aa
jBituin <bituinjoemel@gmail.com>
2021-05-15 03:26:59 +0800

    Update Features

--- components/Features/component.tsx
+++ components/Features/component.tsx
@@ -1,266 +1,132 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Transition } from '@headlessui/react';
+import React from 'react';
 
 export const Features: React.FC = () => {
-  const [tab, setTab] = useState(1);
-
-  const tabs = useRef(null);
-
-  const heightFix = () => {
-    if (tabs.current.children[tab]) {
-      tabs.current.style.height =
-        tabs.current.children[tab - 1].offsetHeight + 'px';
-    }
-  };
-
-  useEffect(() => {
-    heightFix();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tab]);
-
   return (
-    <section className='relative'>
-      {/* Section background (needs .relative class on parent and next sibling elements) */}
-      <div
-        className='absolute inset-0 bg-gray-100 pointer-events-none mb-16'
-        aria-hidden='true'
-      ></div>
-      <div className='absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2'></div>
-
-      <div className='relative max-w-6xl mx-auto px-4 sm:px-6'>
-        <div className='pt-12 md:pt-20'>
-          {/* Section header */}
-          <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
-            <h1 className='h2 mb-4'>Explore the solutions</h1>
-            <p className='text-xl text-gray-600'>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur excepteur sint occaecat
-              cupidatat.
-            </p>
-          </div>
-
-          {/* Section content */}
-          <div className='md:grid md:grid-cols-12 md:gap-6'>
-            {/* Content */}
-            <div
-              className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6'
-              data-aos='fade-right'
+    <div className='container mx-auto px-6 p-6 bg-white dark:bg-gray-800'>
+      <div className='mb-16 text-center'>
+        <h2 className='text-base text-indigo-600 font-semibold tracking-wide uppercase'>
+          Features
+        </h2>
+        <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl'>
+          A better way to live
+        </p>
+      </div>
+      <div className='flex flex-wrap my-12 dark:text-white'>
+        <div className='w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8'>
+          <div className='flex items-center mb-6'>
+            <svg
+              width='20'
+              height='20'
+              fill='currentColor'
+              className='h-6 w-6 text-indigo-500'
+              viewBox='0 0 1792 1792'
+              xmlns='http://www.w3.org/2000/svg'
             >
-              <div className='md:pr-4 lg:pr-12 xl:pr-16 mb-8'>
-                <h3 className='h3 mb-3'>Powerful suite of tools</h3>
-                <p className='text-xl text-gray-600'>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa.
-                </p>
-              </div>
-              {/* Tabs buttons */}
-              <div className='mb-8 md:mb-0'>
-                <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
-                    tab !== 1
-                      ? 'bg-white shadow-md border-gray-200 hover:shadow-lg'
-                      : 'bg-gray-200 border-transparent'
-                  }`}
-                  href='#0'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(1);
-                  }}
-                >
-                  <div>
-                    <div className='font-bold leading-snug tracking-tight mb-1'>
-                      Building the Simple ecosystem
-                    </div>
-                    <div className='text-gray-600'>
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
-                    </div>
-                  </div>
-                  <div className='flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3'>
-                    <svg
-                      className='w-3 h-3 fill-current'
-                      viewBox='0 0 12 12'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path d='M11.953 4.29a.5.5 0 00-.454-.292H6.14L6.984.62A.5.5 0 006.12.173l-6 7a.5.5 0 00.379.825h5.359l-.844 3.38a.5.5 0 00.864.445l6-7a.5.5 0 00.075-.534z' />
-                    </svg>
-                  </div>
-                </a>
-                <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
-                    tab !== 2
-                      ? 'bg-white shadow-md border-gray-200 hover:shadow-lg'
-                      : 'bg-gray-200 border-transparent'
-                  }`}
-                  href='#0'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(2);
-                  }}
-                >
-                  <div>
-                    <div className='font-bold leading-snug tracking-tight mb-1'>
-                      Building the Simple ecosystem
-                    </div>
-                    <div className='text-gray-600'>
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
-                    </div>
-                  </div>
-                  <div className='flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3'>
-                    <svg
-                      className='w-3 h-3 fill-current'
-                      viewBox='0 0 12 12'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z'
-                        fillRule='nonzero'
-                      />
-                    </svg>
-                  </div>
-                </a>
-                <a
-                  className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${
-                    tab !== 3
-                      ? 'bg-white shadow-md border-gray-200 hover:shadow-lg'
-                      : 'bg-gray-200 border-transparent'
-                  }`}
-                  href='#0'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(3);
-                  }}
-                >
-                  <div>
-                    <div className='font-bold leading-snug tracking-tight mb-1'>
-                      Building the Simple ecosystem
-                    </div>
-                    <div className='text-gray-600'>
-                      Take collaboration to the next level with security and
-                      administrative features built for teams.
-                    </div>
-                  </div>
-                  <div className='flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3'>
-                    <svg
-                      className='w-3 h-3 fill-current'
-                      viewBox='0 0 12 12'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z'
-                        fill='#191919'
-                        fillRule='nonzero'
-                      />
-                    </svg>
-                  </div>
-                </a>
-              </div>
-            </div>
-
-            {/* Tabs items */}
-            <div
-              className='max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1'
-              data-aos='zoom-y-out'
-              ref={tabs}
-            >
-              <div className='relative flex flex-col text-center lg:text-right'>
-                {/* Item 1 */}
-                <Transition
-                  show={tab === 1}
-                  appear={true}
-                  className='w-full'
-                  enter='transition ease-in-out duration-700 transform order-first'
-                  enterFrom='opacity-0 translate-y-16'
-                  enterTo='opacity-100 translate-y-0'
-                  leave='transition ease-in-out duration-300 transform absolute'
-                  leaveFrom='opacity-100 translate-y-0'
-                  leaveTo='opacity-0 -translate-y-16'
-                >
-                  <div className='relative inline-flex flex-col'>
-                    <img
-                      className='md:max-w-none mx-auto rounded'
-                      src={require('../images/features-bg.png').default}
-                      width='500'
-                      height='462'
-                      alt='Features bg'
-                    />
-                    <img
-                      className='md:max-w-none absolute w-full left-0 transform animate-float'
-                      src={require('../images/features-element.png').default}
-                      width='500'
-                      height='44'
-                      alt='Element'
-                      style={{ top: '30%' }}
-                    />
-                  </div>
-                </Transition>
-                {/* Item 2 */}
-                <Transition
-                  show={tab === 2}
-                  appear={true}
-                  className='w-full'
-                  enter='transition ease-in-out duration-700 transform order-first'
-                  enterFrom='opacity-0 translate-y-16'
-                  enterTo='opacity-100 translate-y-0'
-                  leave='transition ease-in-out duration-300 transform absolute'
-                  leaveFrom='opacity-100 translate-y-0'
-                  leaveTo='opacity-0 -translate-y-16'
-                >
-                  <div className='relative inline-flex flex-col'>
-                    <img
-                      className='md:max-w-none mx-auto rounded'
-                      src={require('../images/features-bg.png').default}
-                      width='500'
-                      height='462'
-                      alt='Features bg'
-                    />
-                    <img
-                      className='md:max-w-none absolute w-full left-0 transform animate-float'
-                      src={require('../images/features-element.png').default}
-                      width='500'
-                      height='44'
-                      alt='Element'
-                      style={{ top: '30%' }}
-                    />
-                  </div>
-                </Transition>
-                {/* Item 3 */}
-                <Transition
-                  show={tab === 3}
-                  appear={true}
-                  className='w-full'
-                  enter='transition ease-in-out duration-700 transform order-first'
-                  enterFrom='opacity-0 translate-y-16'
-                  enterTo='opacity-100 translate-y-0'
-                  leave='transition ease-in-out duration-300 transform absolute'
-                  leaveFrom='opacity-100 translate-y-0'
-                  leaveTo='opacity-0 -translate-y-16'
-                >
-                  <div className='relative inline-flex flex-col'>
-                    <img
-                      className='md:max-w-none mx-auto rounded'
-                      src={require('../images/features-bg.png').default}
-                      width='500'
-                      height='462'
-                      alt='Features bg'
-                    />
-                    <img
-                      className='md:max-w-none absolute w-full left-0 transform animate-float'
-                      src={require('../images/features-element.png').default}
-                      width='500'
-                      height='44'
-                      alt='Element'
-                      style={{ top: '30%' }}
-                    />
-                  </div>
-                </Transition>
-              </div>
-            </div>
+              <path d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'></path>
+            </svg>
+            <div className='ml-4 text-xl'>Increase sales</div>
           </div>
+          <p className='leading-loose text-gray-500 dark:text-gray-200 text-md'>
+            Receive more sales by selling across multple sales channels instead
+            of just having a single point of entry.
+          </p>
+        </div>
+        <div className='w-full border-b md:w-1/2 lg:w-1/3 lg:border-r p-8'>
+          <div className='flex items-center mb-6'>
+            <svg
+              width='20'
+              height='20'
+              fill='currentColor'
+              className='h-6 w-6 text-indigo-500'
+              viewBox='0 0 1792 1792'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'></path>
+            </svg>
+            <div className='ml-4 text-xl'>Overlays</div>
+          </div>
+          <p className='leading-loose text-gray-500 dark:text-gray-200 text-md'>
+            Apply beautiful overlays to every product image distributed through
+            our platform. A visual touch.
+          </p>
+        </div>
+        <div className='w-full border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0 p-8'>
+          <div className='flex items-center mb-6'>
+            <svg
+              width='20'
+              height='20'
+              fill='currentColor'
+              className='h-6 w-6 text-indigo-500'
+              viewBox='0 0 1792 1792'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'></path>
+            </svg>
+            <div className='ml-4 text-xl'>Control</div>
+          </div>
+          <p className='leading-loose text-gray-500 dark:text-gray-200 text-md'>
+            Apply filters and control which products to sell on each sales
+            channel. E.g. exclude products with low margins.
+          </p>
+        </div>
+        <div className='w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8'>
+          <div className='flex items-center mb-6'>
+            <svg
+              width='20'
+              height='20'
+              fill='currentColor'
+              className='h-6 w-6 text-indigo-500'
+              viewBox='0 0 1792 1792'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'></path>
+            </svg>
+            <div className='ml-4 text-xl'>Mapping</div>
+          </div>
+          <p className='leading-loose text-gray-500 dark:text-gray-200 text-md'>
+            Map product categories with each sales channels&#x27; own categories
+            and achieve better results and lower costs.
+          </p>
+        </div>
+        <div className='w-full border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0 p-8'>
+          <div className='flex items-center mb-6'>
+            <svg
+              width='20'
+              height='20'
+              fill='currentColor'
+              className='h-6 w-6 text-indigo-500'
+              viewBox='0 0 1792 1792'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'></path>
+            </svg>
+            <div className='ml-4 text-xl'>Fill the missing</div>
+          </div>
+          <p className='leading-loose text-gray-500 dark:text-gray-200 text-md'>
+            Modify products with extra properties and achieve the maximum output
+            for each installed sales channel.
+          </p>
+        </div>
+        <div className='w-full md:w-1/2 lg:w-1/3 p-8'>
+          <div className='flex items-center mb-6'>
+            <svg
+              width='20'
+              height='20'
+              fill='currentColor'
+              className='h-6 w-6 text-indigo-500'
+              viewBox='0 0 1792 1792'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path d='M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z'></path>
+            </svg>
+            <div className='ml-4 text-xl'>Dynamic Texts</div>
+          </div>
+          <p className='leading-loose text-gray-500 dark:text-gray-200 text-md'>
+            Build unique product titles and descriptions instead of spending
+            days manually editing each product.
+          </p>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
